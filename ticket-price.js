f9932892f98e54f7e20477cac6413779155d1393
unknown <7b7bruno@gmail.com>
2025-10-27 10:53:35 +0200

    Added ticket price check.

---
+++ ticket-price.js
@@ -0,0 +1,13 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (visitorAge <= 6) {
+    console.log("Ieeja ir bez maksas.");
+}
+else {
+    if (isStudent == true) {
+        console.log("Biļetes cena ir 5 EUR.");
+    } else {
+        console.log("Biļetes cena ir 10 EUR");
+    }
+};
