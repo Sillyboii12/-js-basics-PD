e05b539f306511f6f436de1d0190789a290c838c
unknown <7b7bruno@gmail.com>
2025-10-27 11:00:25 +0200

    Log out people names

---
+++ people-array.js
@@ -0,0 +1,10 @@
+let people = [
+    {name: "Adrians", age: 16, isStudent: true},
+    {name: "Anna", age: 12, isStudent: false}
+];
+
+let person;
+for (let i = 0 ; i< people.length; i++) {
+    person = people[i];
+    console.log("Vārds:", person.name);
+};
