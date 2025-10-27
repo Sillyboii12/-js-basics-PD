fa6666e48e8563ca046ab4f8208909e4965bb485
unknown <7b7bruno@gmail.com>
2025-10-27 10:47:59 +0200

    Fixed

--- iterations.js
+++ iterations.js
@@ -2,10 +2,10 @@ for (let i = 21; i<= 30; i++) {
     console.log(i);
 }
 
-console.log("==================")
-let n = 30
+console.log("==================");
+let n = 30;
 
 while (n > 21) {
-    n -= 1
-    console.log(n)
+    n -= 1;
+    console.log(n);
 }
